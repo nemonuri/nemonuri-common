0fc24de19f61e98deeb02800bf74eca7daf67608
nemonuri <nemonuri2@gmail.com>
2024-09-06 18:01:00 +0900

    add nongeneric IReceiver

--- src/Nemonuri.Common.Primitives/IReceiver.cs
+++ src/Nemonuri.Common.Primitives/IReceiver.cs
@@ -1,6 +1,11 @@
 namespace Nemonuri;
 
-public interface IReceiver<T>
+public interface IReceiver
+{
+    void OnReceived(object? provider, object received);
+}
+
+public interface IReceiver<T> : IReceiver
 {
     void OnReceived(object? provider, T received);
 }
