03f074f14f66ad81f67dcc6e61f940db9ed0f4d9
nemonuri <nemonuri2@gmail.com>
2024-09-06 19:02:01 +0900

    add ReceiverPolyfill

---
+++ src/Nemonuri.Common.Primitives/Primitives/ReceiverPolyfill.cs
@@ -0,0 +1,36 @@
+namespace Nemonuri.Primitives;
+
+public static class ReceiverPolyfill
+{
+    public static void PolyfillOnReceived<T>
+    (
+        IReceiver<T> source,
+        object? provider, object received,
+        Action<object?, object>? fallback = null
+    )
+    {
+        Guard.IsNotNull(source);
+        Guard.IsNotNull(received);
+
+        if (received is T vT)
+        {
+            source.OnReceived(provider, vT);
+        }
+        else if (fallback != null)
+        {
+            fallback(provider, received);
+        }
+        else
+        {
+            throw new InvalidOperationException
+            (
+                string.Format
+                (
+                    "Type mismatched. expected: {0}, actual: {1}", 
+                    typeof(T),
+                    received.GetType()
+                )
+            );
+        }
+    }
+}
